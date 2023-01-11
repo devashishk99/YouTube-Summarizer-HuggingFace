c09ad10cce538211db1cff53cf986c4d6743c5c4
Devashish Kamble <35958174+devashishk99@users.noreply.github.com>
2023-01-11 18:32:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
-from transformers import pipeline
-from youtube_transcript_api import YouTubeTranscriptApi
-import streamlit as st
-from pytube import YouTube
+from transformers import pipeline #the holy grail of all pre-trained LLMs
+from youtube_transcript_api import YouTubeTranscriptApi #the python API which allows you to get the transcripts/subtitles for a given YouTube video 
+import streamlit as st #open-source framework to build quick webapps
+from pytube import YouTube #library for downloading YouTube Videos
 
+#runs the function and stores the model in a local cache
+#helpful when working large ml models
 @st.cache(allow_output_mutation=True)
 def load_summarizer():
     model = pipeline("summarization")
     return model
 
+#takes the video url as input and returns video id along with displaying thumbnail & title of video
 def get_video_metadata(url):
     yt = YouTube(url)
     st.image(yt.thumbnail_url)
     st.header(yt.title)
     return url.split("=")[1]
 
+#generates transcript using the api 
 def get_video_transcript(video_id):
     transcript = YouTubeTranscriptApi.get_transcript(video_id)
     result = ""
@@ -21,6 +25,7 @@ def get_video_transcript(video_id):
         result += ' ' + i['text']
     return result
 
+#breaks down large text blob into chunks so LLMs have limited processing capability
 def generate_text_chunks(text):
     res = []
     num_iters = int(len(text)/1000)
